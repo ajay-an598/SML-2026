2c918e493154fb8a62796733e585ad6ba1014076
ajay-an598 <an598@student.london.ac.uk>
2026-05-31 06:32:17 +0000

    replaced InstructionTestSupport with coursework description version to make initial build pass

--- src/test/java/sml/instructions/InstructionTestSupport.java
+++ src/test/java/sml/instructions/InstructionTestSupport.java
@@ -1,3 +1,52 @@
+// package sml.instructions;
+
+// import sml.Instruction;
+// import sml.Machine;
+
+// import java.lang.reflect.Constructor;
+
+// /**
+//  * Helper class used by the provided instruction unit tests.
+//  *
+//  * This file should be placed in:
+//  *   sml/src/test/java/sml/instructions/InstructionTestSupport.java
+//  */
+// final class InstructionTestSupport {
+
+//     private InstructionTestSupport() {
+//         // Utility class: do not instantiate.
+//     }
+
+//     static Machine machine() {
+//         return new Machine();
+//     }
+
+//     static Instruction instruction(String name, int... args) {
+//         try {
+//             String className = "sml.instructions." + name + "Instruction";
+//             Class<?> clazz = Class.forName(className);
+
+//             Class<?>[] parameterTypes = new Class<?>[args.length + 1];
+//             Object[] values = new Object[args.length + 1];
+
+//             parameterTypes[0] = String.class;
+//             values[0] = "test";
+
+//             for (int i = 0; i < args.length; i++) {
+//                 parameterTypes[i + 1] = int.class;
+//                 values[i + 1] = args[i];
+//             }
+
+//             Constructor<?> constructor = clazz.getConstructor(parameterTypes);
+//             return (Instruction) constructor.newInstance(values);
+
+//         } catch (Exception e) {
+//             throw new RuntimeException("Could not create instruction: " + name, e);
+//         }
+//     }
+// }
+
+//pasted from coursework description
 package sml.instructions;
 
 import sml.Instruction;
@@ -17,8 +66,10 @@ final class InstructionTestSupport {
         // Utility class: do not instantiate.
     }
 
-    static Machine machine() {
-        return new Machine();
+    static Machine machine() { 
+        Machine machine = new Machine();
+        machine.registers(new sml.Registers());
+        return machine;
     }
 
     static Instruction instruction(String name, int... args) {
